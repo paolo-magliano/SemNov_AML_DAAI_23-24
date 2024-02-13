7ced84f24fb14a7bd965fa1a4bd580882e8f0a73
Lorenzo Ugoccioni <126766628+LorenzoUgo@users.noreply.github.com>
2024-02-13 20:46:03 +0100

    temporary

--- utils/ood_utils.py
+++ utils/ood_utils.py
@@ -435,12 +435,11 @@ def get_ood_metrics(src_scores, tar_scores, src_names, tar_names, src_label=1):
     names = np.concatenate([src_names, tar_names], axis=0)
     res = calc_metrics(scores, labels)
 
-    f1_fail_names = [names[i] for i in range(len(names)) if scores[i] < res['f1_threshold']]
-    print(f1_fail_names, "\n", f1_fail_names.shape)
-    f1_fail_counter = collections.Counter(f1_fail_names) 
-    j_fail_names = [names[i] for i in range(len(names)) if scores[i] < res['j_threshold']]
-    print(j_fail_names[0])
-
+    f1_fail_names = [(names[i][0], names[i][1]) for i in range(len(names)) if scores[i] < res['f1_threshold']]
+    print(f1_fail_names, "\n", f1_fail_names[0])
+    f1_fail_counter = collections.Counter(f1_fail_names)
+    
+    j_fail_names = [(names[i][0], names[i][1]) for i in range(len(names)) if scores[i] < res['j_threshold']]
     j_fail_counter = collections.Counter(j_fail_names)
 
     print(f"OOD F1 Test - Acc: {res['f1_accuracy']:.4f}, Th: {res['f1_threshold']:.4f}, Fail: {len(f1_fail_names)}")
