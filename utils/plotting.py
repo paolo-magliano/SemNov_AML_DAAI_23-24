623a166647502dc1c8be30157417e71378933ded
Lorenzo Ugoccioni <126766628+LorenzoUgo@users.noreply.github.com>
2024-02-17 16:46:55 +0100

    Update plotting.py

--- utils/plotting.py
+++ utils/plotting.py
@@ -3,9 +3,22 @@ import matplotlib.pyplot as plt
 import open3d as o3d
 import imageio.v3 as iio
 
+def load():
+    f = open("/content/3D_OS_release_data/modelnet40_normal_resampled/airplane_0001.txt", "r")
+    matrix = []
+    for l in f:
+        # use try-except block to protect the execution
+        line = l.rstrip().split(',')
+        line = line[:3]
+        matrix.append(line)
+
+    return matrix
+
+
 # Create Figure:
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-ax.scatter3D(pcd[:, 0], pcd[:, 1], pcd[:, 2])
+figure = load()
+ax.scatter3D(figure[:, 0], figure[:, 1], figure[:, 2])
 # label the axes
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
